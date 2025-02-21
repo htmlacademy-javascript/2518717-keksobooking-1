ec2f8b3adb2f997e553db7b2d7c46f202ce74c6f
Keks <keks@htmlacademy.ru>
2025-02-21 05:50:52 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,13 @@
+import {renderPopup} from './generate-markup.js';
+import { generateRooms } from './data.js';
+import { disableForm, enableForm, disableFiltr, enableFiltr } from './control-form.js';
+import './validate-form.js';
+
+const estateObjects = generateRooms(1);
+renderPopup (estateObjects[0]);
+
+disableForm();
+enableForm();
+
+disableFiltr();
+enableFiltr();
